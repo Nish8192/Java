84bfce503cba6616c855828b1cad31427ff68672
nish8192 <nish8192@gmail.com>
2017-07-07 11:47:16 -0400

    Robot constructor experiments

--- Classes/src/examples/Robot.java
+++ Classes/src/examples/Robot.java
@@ -2,17 +2,38 @@ package examples;
 
 public class Robot {
 	private int maxSpeed;
+	private String name;
 	
 	public Robot() {
 		this.maxSpeed = 21;
+		this.name = "Nishant";
 	}
 	
 	public Robot(int startSpeed) {
 		this.maxSpeed = startSpeed;
+		this.name = "Nishant";
+	}
+	
+	public Robot(int startSpeed, String name) {
+		this.maxSpeed = startSpeed;
+		this.name = name;
+	}
+	
+	public Robot(String name) {
+		this.name = name;
+		this.maxSpeed = 21;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 	public int getMaxSpeed() {
-		return maxSpeed;
+		return this.maxSpeed;
 	}
 	
 	public void setMaxSpeed(int speed) {
@@ -26,7 +47,7 @@ public class Robot {
 	}
 	
 	public void travel() {
-		System.out.println("I'm moving " + this.maxSpeed + "!! So FAST!!");
+		System.out.println(this.name + " is moving at " + this.maxSpeed + "!! So FAST!!");
 	}
 
 }
--- Classes/src/examples/RobotTester.java
+++ Classes/src/examples/RobotTester.java
@@ -2,8 +2,8 @@ package examples;
 
 public class RobotTester {
 	public static void main(String[] args) {
-		Robot nishant = new Robot();
-		Robot poop = new Robot(56);
+		Robot nishant = new Robot(100, "Nishant");
+		Robot poop = new Robot("poop");
 		nishant.travel();
 		poop.travel();
 	}
