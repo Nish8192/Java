eeefcac180c6a53800d2f0adb46b2696a065e1b4
nish8192 <nish8192@gmail.com>
2017-07-10 16:01:05 -0400

    working with Servlet and JSP and Spring

---
+++ Spring/ServletJSPExample/src/com/servlet/example/ServletExample.java
@@ -0,0 +1,35 @@
+package com.servlet.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletExample
+ */
+@WebServlet("/helloServlet")
+public class ServletExample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletExample() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter output = response.getWriter();
+		output.write("A Dynamic Web Application Servlet!!!");
+	}
+
+}
